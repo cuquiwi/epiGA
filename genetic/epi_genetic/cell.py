8fa8512c331d410eac32fb19a0bbf59619c745c9
Albert <albertcardenete95@gmail.com>
2019-01-07 13:35:26 +0100

    Added nucleosome_generation function

--- genetic/epi_genetic/cell.py
+++ genetic/epi_genetic/cell.py
@@ -2,11 +2,11 @@ import numpy as np
 
 class Cell:
 
-    def __init__(self, solution, father = None, mother = None, mask = None):
+    def __init__(self, solution, father = None, mother = None, nucleosome = None):
         self.solution = solution
         self.father = father
         self.mother = mother
-        if mask == None:
-            self.mask = np.zeros(len(solution))
+        if nucleosome == None:
+            self.nucleosome = np.zeros(len(solution))
         else:
-            self.mask = mask
+            self.nucleosome = nucleosome
--- genetic/epi_genetic/epigen.py
+++ genetic/epi_genetic/epigen.py
@@ -1,4 +1,4 @@
-from random import shuffle
+from random import shuffle, random
 from cell import Cell
 
 def epigen_alg(individualsNb, cellsNb, epiProb, nucleoProb, nucleoRad, mechanisms, environment, max_epoch = 500):
@@ -34,19 +34,43 @@ def termination(i, max_epoch):
     Termination condition for the EpiGA.
     Inputs:
         - i: Current iteration of the algorithm.
-        - max_epoch: 
+        - max_epoch: Maximum number of epochs.
+    Output:
+        True if the termination condition is accomplished, False
+        otherwise.
     """
+    #TODO: Hacer una super funcion de terminacion. Discuss why.
     if i>=max_epoch:
         return True
     else:
         return False
 
+def evaluate_cell(solution):
+    """
+    Function that evaluates the fitness of our problem for a given cell.
+    Inputs:
+        - solution: a cell which contains the solution.
+    Outputs:
+        The fitness value of a given cell.
+    """
+    #TODO: Hacer la funcion que evalua el problema
+    return 3.1415926535
+
 def init_population(individualsNb, cellsNb):
+    """
+    The initial population of the EpiGA.
+    Inputs:
+        - individualsNb: Number of initial individuals in the problem.
+        - cellsNb: Number of cells per each individual.
+    Return:
+        A list of cell elements representing the initial population.
+    """
     population = []
     for i in range(individualsNb):
         individual = []
         for j in range(cellsNb):
-            solution = shuffle([k+1 for k in range(128)]) #TODO generalize solution for other problems than a280
+            solution = [k+1 for k in range(128)]
+            shuffle(solution) #TODO generalize solution for other problems than a280
             individual.append(Cell(solution))
 
         population.append(individual)
@@ -54,12 +78,54 @@ def init_population(individualsNb, cellsNb):
     return population
 
 def selection(population):
-    #TODO
+    """
+    Performs the population selection in the EpiGA.
+    Inputs:
+        - population: the total population.
+    Return:
+        THe selected subset of the total population.
+    """
+    #TODO: Binary tournment or roulette. Discuss why we have chosen a particular implementation.
     return population
 
 def nucleosome_generation(population, prob, radius):
-    #TODO
+    """
+    Generates a new nucleosome vector as a mask for each cell in the 
+    individuals of the population.
+    Inputs:
+        - population: the total population.
+        - prob: Nucleosome generation probability.
+        - radius: Radius of the nucleosome.
+    Output:
+        The population with the new nucleosomes generated.
+    """
+    for i in range(len(population)):
+        individual = population[i]
+        for j in range(len(individual)):
+            cells = individual[j]
+            n = cells.mask
+            for k in range(len(n)):
+                if random() < prob:
+                    n = collapse(n,radius,k)
+            cells.mask = n
+            individual[j] = cells
+        population[i] = individual
     return population
+
+def collapse(nucleosome, radius, k):
+    """
+    Function that creates a new nucleosome.
+    Inputs:
+        - nucleosome: The binary list of the cell that represents the nucleosome.
+        - radius: trivial.
+        - k: the center position of the new nucleosome.
+    Output:
+        The new modified nucleosome.
+    """
+    # TODO: Checkear si esto es correcto, o me lo he patillado. Pag. 257.
+    for i in range(-radius, radius+1,1):
+        nucleosome[k+i] = 1
+    return nucleosome
 
 def nucleosome_reproduction(population):
    #TODO
