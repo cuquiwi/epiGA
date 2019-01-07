925a2d161d7309a83adab8362c877c7fde60e4b1
Albert <albertcardenete95@gmail.com>
2019-01-07 10:32:39 +0100

    Added some docstrings

--- genetic/epi_genetic/epigen.py
+++ genetic/epi_genetic/epigen.py
@@ -1,18 +1,44 @@
 from random import shuffle
 from cell import Cell
 
-def epigen_alg(individualsNb, cellsNb, epiProb, nucleoProb, nucleoRad, max_epoch = 500):
+def epigen_alg(individualsNb, cellsNb, epiProb, nucleoProb, nucleoRad, mechanisms, environment, max_epoch = 500):
+    """
+    EpiGA based on the work by D.H. Stolfi and E. Alba, (2017).
+    Inputs:
+        individualsNb: Number of individuals in the problem.
+        cellsNb: Number of cells per each individual.
+        epiProb: Epigenetic probabilities list.
+        nucleoProb: Nucleosome probability.
+        nucleoRad: Nucleosome radius.
+        mechanisms: List of string containing the epigenetic algorithms
+                    in the same order as the epiProb list.
+        environment: Environment rules
+    """
     population = init_population(individualsNb, cellsNb)
     aux_population = []
     i = 0
-    while i < max_epoch: #TODO add termination condition
+    termination_condition = False
+    while not termination_condition:
         newpop = selection(population[i])
         newpop = nucleosome_reproduction(newpop)
         newpop = epigen_mechanism(newpop, epiProb)
 
         aux_population.append(newpop)
         population.append(replacement(population[i], newpop))
+        termination_condition = termination(i, max_epoch)
         i = i+1
+
+def termination(i, max_epoch):
+    """
+    Termination condition for the EpiGA.
+    Inputs:
+        - i: Current iteration of the algorithm.
+        - max_epoch: 
+    """
+    if i>=max_epoch:
+        return True
+    else:
+        return False
 
 def init_population(individualsNb, cellsNb):
     population = []
